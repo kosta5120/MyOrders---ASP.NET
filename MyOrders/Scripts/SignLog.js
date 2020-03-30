d5310b17fef0e8d25c74fd53f91dcab053eee4eb
Kosta Sidorenko <Kosta.sidorenko@quality-line.org>
2020-03-30 08:53:57 +0300

    add login page

---
+++ MyOrders/Scripts/SignLog.js
@@ -0,0 +1,22 @@
+﻿
+
+document.getElementById("tab-signup").addEventListener("click", LogIn);
+document.getElementById("tab-login").addEventListener("click", SignUp);
+
+
+document.getElementById("login").style.display = "none";
+
+function SignUp() {
+
+    document.getElementById("signup").style.display = "none";
+    document.getElementById("login").style.display = "block";
+    
+    
+}
+function LogIn() {
+    document.getElementById("signup").style.display = "block";
+    document.getElementById("login").style.display = "none";
+    var x = document.getElementById("tab-signup");
+    x.satyle.backgroundColor = "blue";
+    
+}
