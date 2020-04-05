700635d16f4255a0845affecac17fac342fca646
Kosta Sidorenko <Kosta.sidorenko@quality-line.org>
2020-04-05 09:30:21 +0300

    Update The JS

--- MyOrders/Scripts/SignLog.js
+++ MyOrders/Scripts/SignLog.js
@@ -10,13 +10,25 @@ function SignUp() {
 
     document.getElementById("signup").style.display = "none";
     document.getElementById("login").style.display = "block";
+
+    var login = document.getElementById("tab-login");
+    var signUp = document.getElementById("tab-signup");
+
+    login.style.background = "#6699ff";
+    signUp.style.background = "#99bbff";
+
     
     
 }
 function LogIn() {
     document.getElementById("signup").style.display = "block";
     document.getElementById("login").style.display = "none";
-    var x = document.getElementById("tab-signup");
-    x.satyle.backgroundColor = "blue";
+
+    var login = document.getElementById("tab-login");
+    var signUp = document.getElementById("tab-signup");
+
+    login.style.background = "#99bbff";
+    signUp.style.background = "#6699ff";
+
     
 }
